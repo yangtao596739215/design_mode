0fa113583c1b1db51fa6137bdb23feb82574441d
taoyang <taoyang@6tiantian.com>
2020-11-26 15:26:30 +0800

    init

---
+++ src/test/java/duguqiubai/xyz/designmode/DesignmodeApplicationTests.java
@@ -0,0 +1,13 @@
+package duguqiubai.xyz.designmode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignmodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
